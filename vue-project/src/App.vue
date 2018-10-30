33c88335fd67c9ee4429d9ba752c2a622926a600
Airat Mufazalov <airat.mufazalov@yandex.ru>
2018-10-30 22:42:10 +0300

    Валидация пароля

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -13,27 +13,63 @@
                 <div class="invalid-feedback" v-if="!$v.email.required">Email field is required</div>
                 <div class="invalid-feedback" v-if="!$v.email.email">This field should be an email</div>
             </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input  type="password"
+                        id="password"
+                        class="form-control"
+                        @blur="$v.password.$touch()"
+                       v-model="password"
+                       :class="{'is-invalid': $v.password.$error}"
+                       >
+                <div class="invalid-feedback" v-if="!$v.password.minLength">
+                    Min length of password is {{ $v.password.$params.minLength.min }}. Now it is {{ password.length }}
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="confirm">Confirm Password</label>
+                <input  type="password"
+                        id="confirm"
+                        class="form-control"
+                        @blur="$v.confirmPassword.$touch()"
+                        v-model="confirmPassword"
+                        :class="{'is-invalid': $v.confirmPassword.$error}"
+                >
+                <div class="invalid-feedback" v-if="!$v.confirmPassword.sameAs">
+                   Passwords should match
+                </div>
+            </div>
         </form>
 
-   <!--     <pre>
-            {{ $v.email }}
-        </pre>-->
+        <!--<pre>-->
+            <!--{{ $v.password }}-->
+        <!--</pre>-->
     </div>
 </template>
 
 <script>
-    import {required, email} from 'vuelidate/lib/validators'
+    import {required, email, minLength, sameAs} from 'vuelidate/lib/validators'
 
     export default {
         data() {
             return {
-                email: ''
+                email: '',
+                password: '',
+                confirmPassword: ''
             }
         },
         validations: {
             email: {
                 required: required,
                 email: email
+            },
+            password: {
+                minLength: minLength(6)
+            },
+            confirmPassword: {
+                sameAs: sameAs('password')
             }
         }
     }
