23f603f13e7cc6cd3129d2ae122bdd73d445bc8e
Airat Mufazalov <airat.mufazalov@yandex.ru>
2018-10-31 20:59:18 +0300

    Динамические роуты

---
+++ vue-project/src/pages/Car.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Car id {{ id }}</h1>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                //id: this.$router.currentRoute.params['id'] //Теущий id передаваемый по ссылке
+                id: this.$route.params['id'] //аналагично к текущему роуту
+            }
+        },
+        //Чтобы снова отрендерился компонент Car при переходе с Car 3 на Car 4 в меню
+        watch: {
+            $route(toR, fromR){
+                this.id = toR.params['id']
+            }
+        }
+    }
+</script>
