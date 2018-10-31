6faf3d0b0144d0562a0c0dbf6a460a17aa32e10f
Airat Mufazalov <airat.mufazalov@yandex.ru>
2018-10-31 20:43:37 +0300

    Создание навигации

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,17 @@
 <template>
     <div class="container">
-        <!--Куда будем рендерить определнный роут-->
-        <!--Рендериться будет тот компонент, который соответствует данному роуту-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse" >
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <router-link class="nav-link" to="/">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/cars">Cars</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         <router-view></router-view>
     </div>
 </template>
