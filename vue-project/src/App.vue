0b56a5553350085096045cb0fc7a3884152f0ba2
Airat Mufazalov <airat.mufazalov@yandex.ru>
2018-11-04 19:21:51 +0300

    Глобальная настройка

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -41,19 +41,12 @@
                 this.resource.save({}, car)
             },
             loadCars(){
-                // this.$http.get('http://localhost:3000/cars')
-                //     .then(response => {
-                //         return response.json()
-                //     })
-                //     .then(cars => {
-                //         this.cars = cars;
-                //     })
                 this.resource.get().then(response => response.json())
                     .then(cars => this.cars = cars);
             }
         },
         created() {
-            this.resource = this.$resource('http://localhost:3000/cars')
+            this.resource = this.$resource('cars')
         }
 
     }
