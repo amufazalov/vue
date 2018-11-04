76742257d8b936307b6500b3ce96e2e9446fe163
Airat Mufazalov <airat.mufazalov@yandex.ru>
2018-11-04 15:06:58 +0300

    Настройка приложения

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import router from './routes'
+import VueResource from 'vue-resource'
 
-Vue.use(VueRouter)
+Vue.use(VueResource);
 
 new Vue({
     el: '#app',
--- vue-project/src/pages/Error.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h1 style="color:#f00">Error 404</h1>
-</template>
-
-<script>
-    export default {
-
-    }
-</script>
